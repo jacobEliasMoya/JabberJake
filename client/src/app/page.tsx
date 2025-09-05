d6de59fea4cff95b1f2467f04a9e85147db7b60b
jacob moya <jemoya1994@Gmail.com>
2025-09-05 00:02:24 -0700

    minor design updates to mobile layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,11 +3,12 @@ import Form from "@/components/organisms/NewUserForm";
 import JakeLogo from "../../public/jabberjake.png";
 import Header from "@/components/layout/Header";
 import Image from "next/image";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex flex-col md:flex-row">
-      <aside className="flex items-start w-full md:w-max border-b-1 md:border-t-0 md:border-r-1 p-3 border-zinc-700/30 ">
+    <main className="w-full min-h-screen h-screen flex flex-col md:flex-row">
+      <aside className="flex items-center md:items-start w-full md:w-max border-b-1 md:border-t-0 md:border-r-1 p-3 border-zinc-700/30 h-1/12 justify-between rounded-b-xl">
         <Image
           src={JakeLogo}
           alt=""
@@ -16,13 +17,15 @@ export default function Home() {
           sizes=""
           placeholder="empty"
         />
+
+        <RxHamburgerMenu className="w-8 h-auto"/>
       </aside>
 
-      <section className="w-full p-2 flex items-center flex-col justify-between relative ">
+      <section className="w-full p-4 pb-14 flex items-end flex-col justify-between relative h-11/12">
         <Header />
 
-        <div className="w-full mx-auto flex items-center flex-col justify-center gap-2 h-full">
-          <h2 className="text-3xl mb-1 font-semibold text-zinc-300 capitalize">
+        <div className="w-full mx-auto flex items-center flex-col justify-end gap-2 h-full">
+          <h2 className="text-xl md:text-3xl mb-1 font-semibold text-zinc-300 capitalize">
             to chat enter a username 😊
           </h2>
 
